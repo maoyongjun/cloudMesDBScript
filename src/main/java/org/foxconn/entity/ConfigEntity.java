a6f8ca6c0d5c65d4b493405f62c14294e442a6cf
maoyongjun <1048755750@qq.com>
2018-05-22 22:14:15 +0800

    修改成从配置文件获取路径

---
+++ src/main/java/org/foxconn/entity/ConfigEntity.java
@@ -0,0 +1,18 @@
+package org.foxconn.entity;
+/**
+* @author:myz
+* @version 1.0 
+* 创建时间：2018年5月22日 下午10:06:11
+*/
+public class ConfigEntity {
+	private String fileDir;
+
+	public String getFileDir() {
+		return fileDir;
+	}
+
+	public void setFileDir(String fileDir) {
+		this.fileDir = fileDir;
+	}
+	
+}
